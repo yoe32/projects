d5f43f0610eb3252954229fe1fd197ff0ab1353d
yoe32 <yoe32ns@hotmail.com>
2024-06-14 00:36:27 -0600

    14-06-2024

--- python/daily_exchange_rate/app.py
+++ python/daily_exchange_rate/app.py
@@ -8,6 +8,19 @@ url = "https://gee.bccr.fi.cr/IndicadoresEconomicos/Cuadros/frmConsultaTCVentani
 tables = pd.read_html(url)
 df = tables[2]
 
-st.table(df)
+#st.table(df)
 
+# Igualar el nombre de las columnas con el primer row de la tabla
+df.columns = df.iloc[0]
+
+# Cambiar el nombre de la ultima columna por temas de tildes
+df = df.rename(columns={df.columns[5] : 'Ultima Actualizacion'})
+
+# Eliminar el primer row para que sea remplazado por los nombres de las columnas
+df = df.drop(0)
+df = df.reset_index(drop=True)
+
+
+#print(df.head())
 print(df)
+
